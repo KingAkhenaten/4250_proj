5280517dfc96b8b92b40b91d52714b27a6ffcb77
Matthew Justis <JUSTISM@ETSU.EDU>
2023-04-11 16:07:17 -0400

    UserProgress class for DB context. No migration yet.

---
+++ ScavengeRUs/Models/Entities/UserProgress.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ScavengeRUs.Models.Entities
+{
+    public class UserProgress 
+    {
+        [Key]
+        public string PhoneNumber { get; set; }
+        [Required]
+        public string FirstName { get; set; } = string.Empty;
+        [Required]
+        public string Task { get; set; } = string.Empty;
+        [Required]
+        public DateTime StartTime { get; set; }
+        [Required]
+        public DateTime EndTime { get; set; }
+        [Required]
+        public int Rank { get; set; }
+
+        [Display(Name = "Access Code")]
+        public string? AccessCode { get; set; }
+        public string? Completed { get; set; }
+        
+    }
+}
