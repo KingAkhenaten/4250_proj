3502f8eddef2352175bed1059f4cb1ed0b2d2fed
chrisseals98 <chrisseals9893@gmail.com>
2023-04-21 14:27:39 -0400

    Added some comments to the Models

--- ScavengeRUs/Models/Entities/HuntLocation.cs
+++ ScavengeRUs/Models/Entities/HuntLocation.cs
@@ -3,7 +3,9 @@
     public class HuntLocation
     {
         /// <summary>
-        /// This is the weak entity for the many to many relationship between Hunt and Location(tasks)
+        /// This model is the weak entity for the many to many relationship between Hunt and Location(tasks).
+        /// It correlates to the HuntLocation table in the database and the properties below match the columns in the database,
+        /// except for the navigation properties.
         /// </summary>
         public int Id { get; set; }
         public int HuntId { get; set; } //Foreign key
